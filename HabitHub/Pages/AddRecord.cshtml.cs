ddc899308059aa81682e3cca95f6be96ae49361d
Rosynant <arrobasin@gmail.com>
2024-03-08 18:16:17 +0100

    Added AddRecord page

---
+++ HabitHub/Pages/AddRecord.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HabitHub.Pages
+{
+    public class AddRecordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
