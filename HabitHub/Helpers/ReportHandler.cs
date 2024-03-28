09998728801042912c8b4f4b6c5bf8c4cbda1590
Rosynant <arrobasin@gmail.com>
2024-03-28 18:37:23 +0100

    Resposiveness adjustments, started Excel exporting feature

---
+++ HabitHub/Helpers/ReportHandler.cs
@@ -0,0 +1,23 @@
+﻿using HabitHub.Data;
+using HabitHub.Models;
+using OfficeOpenXml;
+
+namespace HabitHub.Helpers;
+
+public static class ReportHandler
+{
+    public static ExcelWorksheet PopulateReport(ExcelWorksheet worksheet, List<HabitModel> habits, List<HabitRecordModel> habitRecords)
+    {
+        worksheet.Cells["A1:D1"].Style.Font.Size = 14.0f;
+        worksheet.Cells["A1:D1"].Style.Font.Bold = true;
+        worksheet.Cells["A1"].Value = "Habit";
+        worksheet.Cells["B1"].Value = "Amount";
+        worksheet.Cells["C1"].Value = "Unit";
+        worksheet.Cells["D1"].Value = "Date";
+
+
+
+        return worksheet;
+    }
+}
+
--- HabitHub/Pages/Index.cshtml.cs
+++ HabitHub/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
+using HabitHub.Data;
+using HabitHub.Helpers;
 using HabitHub.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.Sqlite;
+using OfficeOpenXml;
 
 namespace HabitHub.Pages
 {
@@ -9,6 +12,7 @@ namespace HabitHub.Pages
     {
         public HabitModel Habit { get; set; }
 
+        private const string ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
         private readonly ILogger<IndexModel> _logger;
         private readonly IConfiguration _configuration;
 
@@ -18,9 +22,29 @@ namespace HabitHub.Pages
             _configuration = configuration;
         }
 
-        public void OnGet()
+        public IActionResult GenerateReport()
         {
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Report");
 
+                List<HabitModel> habits = new List<HabitModel>();
+                List<HabitRecordModel> habitRecords = new List<HabitRecordModel>();
+
+                using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+                {
+                    connection.Open();
+                    HabitsRepository.GetAllHabits(connection, habits);
+                    HabitsRepository.GetAllHabitRecords(connection, habitRecords);
+                }
+
+                worksheet = ReportHandler.PopulateReport(worksheet, habits, habitRecords);
+
+                var excelData = package.GetAsByteArray();
+                string fileName = $"habithub_{DateTime.Now.ToString()}.xlsx";
+
+                return File(excelData, ContentType, fileName);
+            }
         }
     }
 }
