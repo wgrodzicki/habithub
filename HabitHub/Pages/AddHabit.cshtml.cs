17ca0a1152684b471ed6b2477c42a5e273ce9fb8
Rosynant <arrobasin@gmail.com>
2024-04-19 18:15:11 +0200

    Reformatting

--- HabitHub/Pages/AddHabit.cshtml.cs
+++ HabitHub/Pages/AddHabit.cshtml.cs
@@ -9,77 +9,77 @@ namespace HabitHub.Pages;
 
 public class AddHabitModel : PageModel
 {
-	[BindProperty] public HabitModel Habit { get; set; }
-	[BindProperty] public string HabitToDelete { get; set; }
+    [BindProperty] public HabitModel Habit { get; set; }
+    [BindProperty] public string HabitToDelete { get; set; }
 
-	public List<string> SavedHabits { get; set; }
-	public string HabitWarning { get; set; } = "";
+    public List<string> SavedHabits { get; set; }
+    public string HabitWarning { get; set; } = "";
 
-	private readonly IConfiguration _configuration;
+    private readonly IConfiguration _configuration;
 
     public AddHabitModel(IConfiguration configuration)
     {
         _configuration = configuration;
-		SavedHabits = new List<string>();
-	}
+        SavedHabits = new List<string>();
+    }
 
     public IActionResult OnGet()
     {
-		using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
-		{
-			connection.Open();
-			HabitsRepository.GetAllHabitNames(connection, SavedHabits);
-		}
-		return Page();
-	}
+        using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+        {
+            connection.Open();
+            HabitsRepository.GetAllHabitNames(connection, SavedHabits);
+        }
+        return Page();
+    }
 
     public IActionResult OnPostAdd()
     {
         if (!ModelState.IsValid)
         {
-			// Validate only the Add habit form
-			foreach (var state in ModelState)
-			{
-				if (state.Key == Habit.HabitName.ToString() && state.Value.ValidationState == ModelValidationState.Invalid)
-					return OnGet();
-			}
+            // Validate only the Add habit form
+            foreach (var state in ModelState)
+            {
+                if (state.Key == Habit.HabitName.ToString() && state.Value.ValidationState == ModelValidationState.Invalid)
+                    return OnGet();
+            }
         }
 
         using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
         {
-			connection.Open();
+            connection.Open();
 
-			// Make sure habit doesn't exist yet
-			if (String.IsNullOrEmpty(HabitsRepository.CheckHabitExists(connection, Habit)))
-			{
-				HabitsRepository.AddHabit(connection, Habit);
-				HabitWarning = "";
-			}
-			else
-			{
-				HabitWarning = "Habit already exists!";
-			}
-		}
-		return OnGet();
-	}
+            // Make sure habit doesn't exist yet
+            if (String.IsNullOrEmpty(HabitsRepository.CheckHabitExists(connection, Habit)))
+            {
+                HabitsRepository.AddHabit(connection, Habit);
+                HabitWarning = "";
+            }
+            else
+            {
+                HabitWarning = "Habit already exists!";
+            }
+        }
+        return OnGet();
+    }
 
-	public IActionResult OnPostDelete()
-	{
-		if (!ModelState.IsValid)
-		{
-			// Validate only the Delete habit form
-			foreach (var state in ModelState)
-			{
-				if (state.Key == nameof(HabitToDelete) && state.Value.ValidationState == ModelValidationState.Invalid)
-					return OnGet();
-			}
-		}
+    public IActionResult OnPostDelete()
+    {
+        if (!ModelState.IsValid)
+        {
+            // Validate only the Delete habit form
+            foreach (var state in ModelState)
+            {
+                if (state.Key == nameof(HabitToDelete) && state.Value.ValidationState == ModelValidationState.Invalid)
+                    return OnGet();
+            }
+        }
 
-		using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
-		{
-			connection.Open();
-			HabitsRepository.DeleteHabit(connection, HabitToDelete);
-		}
-		return OnGet();
-	}
+        using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+        {
+            connection.Open();
+            HabitsRepository.DeleteHabit(connection, HabitToDelete);
+        }
+        return OnGet();
+    }
 }
