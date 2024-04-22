b8ed29953af1f03e81d612e12b954f90e76b7e2c
Rosynant <arrobasin@gmail.com>
2024-04-22 17:18:25 +0200

    Swapped sorting buttons, cleared db

--- HabitHub/Models/HabitRecordModel.cs
+++ HabitHub/Models/HabitRecordModel.cs
@@ -1,9 +1,11 @@
-﻿namespace HabitHub.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace HabitHub.Models;
 
 public class HabitRecordModel
 {
     public int Id { get; set; }
     public int HabitsId { get; set; }
+    [Range(0, float.MaxValue, ErrorMessage = "Value for {0} must be positive")]
     public float Amount { get; set; }
     public string Unit { get; set; }
     public DateTime Date { get; set; }
